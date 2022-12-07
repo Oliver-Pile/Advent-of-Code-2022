b0be8d0aa50cf63c8ce7cae569d54170bc0d7265
Oliver Pile <oliverpile02@gmail.com>
2022-12-07 22:22:43 +0000

    WIP: Trying new search method for getting values

--- Day7/day7.py
+++ Day7/day7.py
@@ -6,19 +6,24 @@ class Tree():
         self.files = {}
         self.parent = parent
         
-
+allNodes = []  
+        
+#Old doesn't work
 def search(dirs,total):
     if len(dirs) != 0:
         for d in dirs:
             total = search(d.children,total)
-            sum = 0
             for f in d.files.keys():
-                sum += int(f)
-            if sum <100000:
-                total += sum
+                total += int(f)
         return total
     else:
         return total
+
+#Loop through dir to find other dirs, then repeat ... (recursive?)
+#Once all dirs found (In list), get all their files and then do the maths
+def newSearch(node):
+    toCheck = [node]
+    
     
 #file = open("Day7\day7.txt")
 file = open("Day7\day7test.txt",'r')
@@ -26,7 +31,7 @@ file = open("Day7\day7test.txt",'r')
 root = Tree("/",None)
 currentNode = root
 for line in file:
-    line = line[:-1]
+    line = line.replace("\n","")
     if(line[0:4] == "$ cd"):
         path = line[5:].strip()
         if(path == ".."):
@@ -47,14 +52,18 @@ for line in file:
                     exists = True
                     break
             if not exists:
-                currentNode.children.append(Tree(dirName,currentNode))
+                newNode = Tree(dirName,currentNode)
+                allNodes.append(newNode)
+                currentNode.children.append(newNode)
         elif(line[0] in string.digits):
             splitFile = line.split(" ")
             if splitFile[1] not in currentNode.files.values():
                 currentNode.files.update({splitFile[0]:splitFile[1]})
 
-           
-total = search(root.children,0)
-print(total)
+vals = {}
+for n in allNodes:
+    val = newSearch(n.children,0)
+    vals.update({n.name:val})
+print(vals)
 print("Yay?")
                 
